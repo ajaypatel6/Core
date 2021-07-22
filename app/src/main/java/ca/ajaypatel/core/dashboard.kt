a5a2ac4d3b084d863f36041ee47da2c20c8f3197
ajaypatel6 <ajaypatel23623@gmail.com>
2021-07-21 20:36:19 -0400

    Main page levels

--- app/src/main/java/ca/ajaypatel/core/dashboard.kt
+++ app/src/main/java/ca/ajaypatel/core/dashboard.kt
@@ -3,9 +3,29 @@ package ca.ajaypatel.core
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//new
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+// need to add     id 'kotlin-android-extensions' to plugins, error
+import kotlinx.android.synthetic.main.activity_main.*
+
 class dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        this.supportActionBar?.hide();
+
+        //BUTTON.setVisibility(View.VISIBLE);
+        levelTwoButton.visibility=View.GONE;
+        levelThreeButton.visibility=View.GONE;
+        levelFourButton.visibility=View.GONE;
     }
 }

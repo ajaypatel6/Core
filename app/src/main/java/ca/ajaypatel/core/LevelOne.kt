aa8098f9aea02ba1ca59382adef19221b3d9c95f
ajaypatel6 <ajaypatel23623@gmail.com>
2021-07-23 21:02:16 -0400

    Main page levels

--- app/src/main/java/ca/ajaypatel/core/LevelOne.kt
+++ app/src/main/java/ca/ajaypatel/core/LevelOne.kt
@@ -7,5 +7,8 @@ class LevelOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level_one)
+        this.supportActionBar?.hide();
+
+
     }
 }
